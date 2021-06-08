4b1071f6baad78124d99e59ed143482d05e3a5c8
Rafael Aquino <rafaaquino28@gmail.com>
2021-06-08 15:47:58 -0300

    Metod update()

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -4,6 +4,7 @@ package com.project.bootcamp.controller;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ public class StockController {
 	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
 	public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto) {
+		return ResponseEntity.ok(dto);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<StockDTO> update(@RequestBody StockDTO dto) {
 		return ResponseEntity.ok(dto);
 	}
 
