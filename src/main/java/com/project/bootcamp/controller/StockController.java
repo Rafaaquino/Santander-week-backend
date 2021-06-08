f6efce634fa796fce2c5971e2a0abc4064676440
Rafael Aquino <rafaaquino28@gmail.com>
2021-06-08 15:42:14 -0300

    Metod save()

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -1,10 +1,22 @@
 package com.project.bootcamp.controller;
 
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.bootcamp.model.dto.StockDTO;
 
 @RestController
 @RequestMapping(value = "/stock")
 public class StockController {
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
+	public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto) {
+		return ResponseEntity.ok(dto);
+	}
 
 }
