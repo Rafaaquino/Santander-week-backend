5d70847d00530db55e2899e873dbe6d0a1089750
Rafael Aquino <rafaaquino28@gmail.com>
2021-06-08 15:59:58 -0300

    Metod findAll()

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -1,8 +1,13 @@
 package com.project.bootcamp.controller;
 
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +28,20 @@ public class StockController {
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<StockDTO> update(@RequestBody StockDTO dto) {
 		return ResponseEntity.ok(dto);
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<StockDTO>> findAll() {
+		List<StockDTO> list = new ArrayList<>();
+		StockDTO dto = new StockDTO();
+		dto.setId(1L);
+		dto.setName("Ethereum");
+		dto.setPrice((double) 12000);
+		dto.setVariation((double) 7);
+		dto.setDate(LocalDate.now());
+		list.add(dto);
+		return ResponseEntity.ok(list);
+				
 	}
 
 }
