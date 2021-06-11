7d8ec1d996e26db8d109cc3c6fcde2ae5ee482ca
Rafael Aquino <rafaaquino28@gmail.com>
2021-06-11 17:07:06 -0300

    Update DB

--- src/main/java/com/project/bootcamp/repository/StockRepository.java
+++ src/main/java/com/project/bootcamp/repository/StockRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.project.bootcamp.model.Stock;
@@ -12,4 +13,9 @@ import com.project.bootcamp.model.Stock;
 public interface StockRepository extends JpaRepository<Stock, Long> {
 	
 	Optional<Stock> findByNameAndDate(String name, LocalDate date);
+	
+	@Query("SELECT stock " +
+			"FROM Stock stock " +
+			"WHERE stock.name = :name AND stock.date = :date AND stock.id <> :id ")
+	Optional<Stock> findByStockUpdate(String name, LocalDate date, Long id);
 }
